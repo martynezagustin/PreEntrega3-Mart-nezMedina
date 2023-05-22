bfce1cff634146460a5a3c6f57a625449fe6c06e
Agustín Martínez Medina <martynezagustin@gmail.com>
2023-05-22 14:35:33 -0300

    creación unitaria de cada curso por el click

--- js/courseDetail.js
+++ js/courseDetail.js
@@ -1,15 +1,21 @@
+//Se referencia al curso en el localStorage
 const cursoReferencia = JSON.parse(localStorage.getItem('cursoSeleccionado'));
 
 const contenedorCurso = document.getElementById('detalle-producto')
 
-console.log(cursoReferencia);
-
+//creación del curso en función del click
 function invocarCurso(){
-    const contenedor = document.createElement("div")
-    for(curso of cursoReferencia){
-        contenedor.innerHTML = `<h2>${curso.nombre}</h2>`
-        contenedorCurso.appendChild(contenedor)
-    }
+    const div = document.createElement('div')
+    div.classList.add('d-flex')
+    div.classList.add('flex-column')
+    div.innerHTML = `<div class="d-flex">
+    <img src="${cursoReferencia.img}" class="w-25 course-img-detail mb-4 mt-4">
+    </div>
+    <h2>${cursoReferencia.nombre}</h2>
+    <h3 class="text-start">${cursoReferencia.duracion}</h3>
+    <h2 class="text-start mt-3">DESCRIPCIÓN</h2>
+    <p>${cursoReferencia.descripcionLarga}</p>`
+    contenedorCurso.appendChild(div)
 }
 
 invocarCurso()
